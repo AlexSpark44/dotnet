143c36a6d661c75331a540b8d770c2a046e4007e
Alex Spark <alter.revolution@icloud.com>
2026-02-20 19:02:53 +0100

    Simplify API startup to always enable Swagger

--- src/Platform.Api/Program.cs
+++ src/Platform.Api/Program.cs
@@ -5,12 +5,7 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.MapGet("/health", () => Results.Ok(new { status = "ok" }));
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.Run();
